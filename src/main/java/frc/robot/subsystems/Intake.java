432c76cdcca7136702f3abe18cc9ff37a95eead8
Evan Thompson <evan.thompson515@gmail.com>
2023-03-28 20:38:04 -0400

    increase intake zone

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -218,7 +218,7 @@ public class Intake {
 
         SmartDashboard.putBoolean("_Breaking Things", robotCommander.getCubeStopIntake());
 
-        if(intakePos == IntakePos.cubeHandoff && Math.abs(angle - intakePos.getPositionReading()) < 10){
+        if(intakePos == IntakePos.cubeHandoff && Math.abs(angle - intakePos.getPositionReading()) < 20){
             speedMotor1.set(TalonFXControlMode.PercentOutput, -1);
             speedMotor2.set(TalonFXControlMode.PercentOutput, 1);
         } else if(!ballSensor.get() && intakeSpeed != IntakeSpeed.out){
